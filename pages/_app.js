c80874aea9bca53c23868ffacbb98391deb7f246
Wilmer Prieto <wilmer.prieto@clevertech.biz>
2022-07-07 08:03:55 -0500

    feat: install react-gtm-module

--- pages/_app.js
+++ pages/_app.js
@@ -1,22 +1,14 @@
+import { useEffect } from 'react';
+import TagManager from 'react-gtm-module';
 import '../styles/globals.css'
-import Script from 'next/script'
 
 function MyApp({ Component, pageProps }) {
+  useEffect(() => {
+    TagManager.initialize({ gtmId: 'G-YHPFPHLDG4' });
+  }, []);
+
   return (
     <>
-      <Script
-        src="https://www.googletagmanager.com/gtag/js?id=G-YHPFPHLDG4"
-        strategy="afterInteractive"
-      />
-      <Script id="google-analytics" strategy="afterInteractive">
-        {`
-          window.dataLayer = window.dataLayer || [];
-          function gtag(){window.dataLayer.push(arguments);}
-          gtag('js', new Date());
-
-          gtag('config', 'G-YHPFPHLDG4');
-        `}
-      </Script>
       <Component {...pageProps} />
     </>
   )
