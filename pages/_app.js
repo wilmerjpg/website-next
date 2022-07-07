312c387824d516cfb4e25a4f1ce416b016b433f6
Wilmer Prieto <wilmer.prieto@clevertech.biz>
2022-07-07 08:27:29 -0500

    feat: add manual GTM

--- pages/_app.js
+++ pages/_app.js
@@ -1,15 +1,29 @@
-import { useEffect } from 'react';
-import TagManager from 'react-gtm-module';
+// import { useEffect } from 'react';
+// import TagManager from 'react-gtm-module';
+import Script from 'next/script'
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
-  useEffect(() => {
-    TagManager.initialize({ gtmId: 'G-YHPFPHLDG4' });
-    console.log("initialized");
-  }, []);
+  // useEffect(() => {
+  //   TagManager.initialize({ gtmId: 'G-YHPFPHLDG4' });
+  //   console.log("Initialized")
+  // }, []);
 
   return (
     <>
+      <Script
+        src="https://www.googletagmanager.com/gtag/js?id=G-YHPFPHLDG4"
+        strategy="afterInteractive"
+      />
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){window.dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'G-YHPFPHLDG4');
+        `}
+      </Script>
       <Component {...pageProps} />
     </>
   )
